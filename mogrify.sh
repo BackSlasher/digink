3e0e693f9cd295702d34a263e92fa8465abccde9
Nitzan Raz <nitz.raz@gmail.com>
2019-11-29 17:41:24 +0200

    No longer morgifying images

--- mogrify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
-RESOLUTION="800x600"
-find digink/images/ -type f -print0 | parallel -0 mogrify -resize "$RESOLUTION"^ -gravity center -extent "$RESOLUTION"
