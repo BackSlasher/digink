599d9b9ccab209727a6978c97558a4f0d8b113b3
Nitzan Raz <nitz.raz@gmail.com>
2019-11-29 18:51:00 +0200

    Divided to files

---
+++ digink/__main__.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+import os
+import math
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+from typing import NamedTuple
+
+from .screens import get_screens
+from .images import get_image
+
+
+
+if __name__ == "__main__":
+    img_path = get_image()
+    img = Image.open(img_path)
+
+    screen_collection = get_screens()
+
+    overall_size = screen_collection.get_overall_size()
+
+    # Restretch the image
+    img = ImageOps.fit(img, overall_size)
+
+    # Display the image on every screen
+    for screen in screen_collection.screens:
+        screen.draw_image(img)
---
+++ digink/screens.py
@@ -0,0 +1,54 @@
+#!/bin/env python
+
+from typing import NamedTuple, List, Tuple
+from IT8951.display import AutoEPDDisplay
+from IT8951 import constants
+
+Display = AutoEPDDisplay
+class Screen(NamedTuple):
+    start_x: int
+    start_y: int
+    width: int
+    height: int
+    display: Display
+
+    def draw_image(self, img):
+        display = self.display
+        display.frame_buf.paste(0xFF, box=(0, 0, self.width, self.height))
+        coords = (self.start_x, self.start_y, self.start_x + self.width, self.start_y + self.height)
+        screen_part = img.crop(coords)
+        display.frame_buf.paste(screen_part)
+        display.draw_full(constants.DisplayModes.GC16)
+
+class ScreenCollection(NamedTuple):
+    screens: List[Screen]
+
+    def get_overall_size(self) -> Tuple[int, int]:
+        max_width = max([screen.width + screen.start_x for screen in self.screens])
+        max_height = max([screen.height + screen.start_y for screen in self.screens])
+        max_dims = (max_width, max_height)
+        return max_dims
+        
+
+def get_screens() -> ScreenCollection:
+    main_display = AutoEPDDisplay(vcom=-2.06)
+    screens = [
+        Screen(
+            start_x=0,
+            start_y=0,
+            width=main_display.width,
+            height=main_display.height,
+            display=main_display,
+        )
+    ]
+    return ScreenCollection(screens)
+
+# Show on screen(s)
+
+# TODO modify IT class to allow custom pins, so we can drive more than one
+# Current pins:
+#     CS    = 8
+#    HRDY  = 24
+#    RESET = 17
+# SPI 0:
+# https://docs.microsoft.com/en-us/windows/iot-core/learn-about-hardware/pinmappings/pinmappingsrpi
